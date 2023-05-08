38d379dad3f8e71af88b08f71cbff9479693b86f
ayassine96 <ali.yassine@uzh.ch>
2023-05-08 11:01:26 +0000

    randomization

--- run_gs_plotting_weekly_final_h1.sh
+++ run_gs_plotting_weekly_final_h1.sh
@@ -2,7 +2,9 @@
 
 # echo "heur 1 Plotting Json & Figures"
 # python3 gs_plotting_weekly_final.py --curr=BTC --heur=1 --freq=week --end=2018-05-12 #--start=2016-07-16
-echo "heur 1 Network Builder"
-python3 network_builder.py --curr=BTC --heur=1 --freq=week --end=2018-05-12 #--start=2009-01-10
-echo "heur 1 Assortativity"
-python3 assortativity_builder.py --curr=BTC --heur=1 --freq=week --end=2018-05-12 #--start=2009-01-10
+# echo "heur 1 Network Builder"
+# python3 network_builder.py --curr=BTC --heur=1 --freq=week --end=2018-05-12 #--start=2009-01-10
+# echo "heur 1 Assortativity"
+# python3 assortativity_builder.py --curr=BTC --heur=1 --freq=week --end=2018-05-12 #--start=2009-01-10
+echo "heur 1 Random Network Builder"
+python3 random_network_builder.py --curr=BTC --heur=1 --freq=week --end=2018-05-12 #--start=2009-01-10
